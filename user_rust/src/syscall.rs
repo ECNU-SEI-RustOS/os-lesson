8b37c44809c61641cbf1fe9f100b05992e269ecb
YCH <12772487+ye-chenhao6@user.noreply.gitee.com>
2025-05-28 00:53:42 +0800

    new syscall in rust

--- user_rust/src/syscall.rs
+++ user_rust/src/syscall.rs
@@ -1,5 +1,13 @@
 use core::{arch::asm};
+const SYSCALL_FORK:usize = 1;
+const SYSCALL_EXIT:usize = 2;
+const SYSCALL_WAIT:usize = 3;
+const SYSCALL_PIPE:usize = 4;
+const SYSCALL_READ:usize = 5;
+const SYSCALL_KILL:usize = 6;
+const SYSCALL_EXEC:usize = 7;
 
+const SYSCALL_WRITE:usize = 16;
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
     unsafe {
@@ -12,4 +20,10 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
         );
     }
     ret
+}
+pub fn sys_read(fd:usize, buffer: &mut [u8])-> isize{
+    syscall(SYSCALL_READ, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
