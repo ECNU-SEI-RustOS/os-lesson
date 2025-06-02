0596076d8bcf0a5dd9c30c00c8545a0ef096190b
YCH <12772487+ye-chenhao6@user.noreply.gitee.com>
2025-06-03 02:27:30 +0800

    test user_program

--- user_rust/src/bin/hello.rs
+++ user_rust/src/bin/hello.rs
@@ -1,11 +1,24 @@
 #![no_std]
 #![no_main]
 
+use user_rust_lib::{ralloc::sbrk, task::sleep};
+
 #[macro_use]
 extern crate user_rust_lib;
 
 #[no_mangle]
 fn main() -> i32 {
     println!("hello world by rust{}",1);
-   0
-}
+    unsafe {
+    let addr = sbrk(0) as usize;
+    println!("{:08x}",addr);
+    let addr = sbrk(100) as usize;
+    println!("{:08x}",addr);
+    let addr = sbrk(-100) as usize;
+    println!("{:08x}",addr);
+    }
+    unsafe {
+        core::arch::asm!("csrrw x0, mstatus, x0"); // 读写 `mstatus` CSR（特权指令）
+    }
+    0
+}   
