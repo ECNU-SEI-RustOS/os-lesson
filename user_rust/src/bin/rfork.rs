bf16390e4da1ac874c38f8705e9325cbf5965bea
YCH <12772487+ye-chenhao6@user.noreply.gitee.com>
2025-06-17 17:32:48 +0800

    change the test

--- user_rust/src/bin/rfork.rs
+++ user_rust/src/bin/rfork.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use user_rust_lib::{task::{fork, sleep, wait}, time::get_mtime};
+use user_rust_lib::{task::{fork, sleep, wait, waitpid}, time::get_mtime};
 
 #[macro_use]
 extern crate user_rust_lib;
@@ -10,16 +10,20 @@ extern crate user_rust_lib;
 fn main(argc:usize, argv:&[&str]) -> i32 {
     let  s= 12;
     let mut exit_code :i32 = 0;
-    if fork() != 0 {
+    let mut child_pid :isize;
+    child_pid = fork();
+    if child_pid != 0 {
+        
+        waitpid(child_pid,&mut exit_code);
+        //wait(&mut exit_code);
         println!("hello world by rust from parent");
+    } else{
+        println!("hello world by rust from child");
         for i in 0..4{
             sleep(10);
             println!("sleep {}s",i);
             println!("{}",get_mtime());
         }
-        wait(&mut exit_code);
-    } else{
-        println!("hello world by rust from child");
     }
     
     0
