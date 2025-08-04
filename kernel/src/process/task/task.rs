f9dfc2c736e18664b4c3c0b9b3f14df29379400c
YCH <12772487+ye-chenhao6@user.noreply.gitee.com>
2025-08-04 15:43:01 +0800

    add new functions in the task mod

--- kernel/src/process/task/task.rs
+++ kernel/src/process/task/task.rs
@@ -1,11 +1,18 @@
-use core::cell::{Cell, UnsafeCell};
+use core::ptr;
+
 use crate::consts::{KERNEL_STACK_SIZE, PAGE_SIZE, USER_STACK_SIZE, TRAMPOLINE, ConstAddr, TRAPFRAME, NPROC};
 use crate::mm::{PhysAddr, RawPage, RawQuadPage, RawSinglePage, VirtAddr};
-use crate::process::{fork_ret, kvm_map};
+use crate::process::{fork_ret};
+use crate::mm::{kvm_task_kstack_map};
 use crate::process::proc::Process;
 use crate::process::trapframe::TrapFrame;
 use crate::process::Context;
+use crate::process::PteFlag;
+use crate::spinlock::SpinLock;
 use super::tid::TID_ALLOCATOR;
+use crate::process::CpuManager;
+use crate::register::satp;
+use crate::trap::user_trap;
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum TaskStatus {
@@ -19,11 +26,6 @@ pub enum TaskStatus {
     Zombie,
 }
 
-/// Return (bottom, top) of a kernel stack in kernel space.
-pub fn kernel_stack_position_by_tid(tid: usize) -> (usize, usize) {
-    let kstack_top: usize = Into::<usize>::into(TRAMPOLINE) - (tid + 1024 + 1) * KERNEL_STACK_SIZE;
-    (kstack_top - KERNEL_STACK_SIZE, kstack_top)
-}
 
 pub struct KernelStack {
     kstack_base: usize,
@@ -35,18 +37,16 @@ impl KernelStack {
 
 pub unsafe fn kstack_alloc(tid: usize) -> KernelStack {
     let (kstack_bottom, kstack_top) = kernel_stack_position_by_tid(tid);
-    // KERNEL_SPACE.exclusive_access().insert_framed_area(
-    //     kstack_bottom.into(),
-    //     kstack_top.into(),
-    //     MapPermission::R | MapPermission::W,
-    // );
     let pa = RawQuadPage::new_zeroed() as usize;
-    // kvm_map(
-    //     VirtAddr::try_from(kstack_bottom).unwrap(),
-    //     PhysAddr::try_from(pa).unwrap(),
-    //     KERNEL_STACK_SIZE,
-    //     PteFlag::R | PteFlag::W,
-    // );
+    kerror!("map kstack {:?} in kernel space",VirtAddr::try_from(kstack_bottom).unwrap());
+    
+    kvm_task_kstack_map(
+        VirtAddr::try_from(kstack_bottom).unwrap(),
+        PhysAddr::try_from(pa).unwrap(),
+        tid,
+        KERNEL_STACK_SIZE,
+        PteFlag::R | PteFlag::W,
+    );
     KernelStack {
         kstack_base: kstack_bottom
     }
@@ -54,7 +54,7 @@ pub unsafe fn kstack_alloc(tid: usize) -> KernelStack {
 
 impl Drop for KernelStack {
     fn drop(&mut self) {
-        
+
     }
 }
 pub struct TaskUserRes {
@@ -69,16 +69,12 @@ impl TaskUserRes {
         process: Option<*mut Process>,
         tid: usize,
         ustack_base: usize,
-        alloc_user_res: bool,
     ) -> Self {
         let task_user_res = Self {
             tid,
             ustack_base,
             process,
         };
-        if alloc_user_res {
-            task_user_res.alloc_user_res();
-        }
         task_user_res
     }
 
@@ -98,13 +94,14 @@ pub struct Task {
     // immutable
     pub process: Option<*mut Process>,
     pub kstack: KernelStack,
+    pub ustack_base: usize,
     pub tid: usize,
     pub pos: usize,
     // mutable
-    inner: UnsafeCell<TaskControlInner>,
+    pub inner: SpinLock<TaskControlInner>,
 }
+
 pub struct TaskControlInner {
-    pub res: Option<TaskUserRes>,
     pub trapframe: *mut TrapFrame,
     pub task_context: Context,
     pub task_status: TaskStatus,
@@ -121,52 +118,46 @@ impl TaskControlInner {
     pub fn set_task_context(&mut self, task_context: Context) {
         self.task_context = task_context
     }
+    pub fn get_context(&mut self) -> *mut Context {
+        &mut self.task_context as *mut Context
+    }
+    pub fn set_status(&mut self, status: TaskStatus){
+        self.task_status = status;
+    }
+    pub fn get_trapframe(&mut self) -> *mut TrapFrame{
+        self.trapframe
+    }
 }
 impl Task {
-    pub fn new(process: Option<*mut Process>, pos:usize, ustack_base: usize, alloc_user_res: bool) -> Self {
+    pub fn new(process: Option<*mut Process>, pos:usize, ustack_base: usize, entry: usize) -> Self {
         let tid = TID_ALLOCATOR.lock().tid_alloc();
-        kinfo!("alloc tid:{}", tid);
-        let res = TaskUserRes::new(process, tid, ustack_base, alloc_user_res);
+        kinfo!("new alloc tid:{}", tid);
         let kstack = unsafe { kstack_alloc(tid) };
-        let proc = unsafe { &mut *process.unwrap() };
+        let proc = unsafe { process.unwrap().as_mut().unwrap() };
         let procdata = proc.data.get_mut();
-        if alloc_user_res {
-            // let pa = unsafe { RawQuadPage::new_zeroed() as usize };
-            // let va_bottom = ustack_bottom_from_tid(ustack_base, tid);
-            // let procdata = proc.data.get_mut();
-            // procdata.pagetable.as_mut().unwrap().uvm_alloc(PGSIZE*5, PGSIZE*10);
-            // procdata.pagetable.as_mut().unwrap().uvm_clear(PGSIZE*10 - PGSIZE *5);
-            // let res = procdata.pagetable.as_mut().unwrap().map_pages(
-            //     VirtAddr::try_from(va_bottom).unwrap(), 
-            //     USER_STACK_SIZE, 
-            //     PhysAddr::try_from(pa).unwrap(), 
-            //     PteFlag::R | PteFlag::W | PteFlag::X | PteFlag::U);
-            // match res {
-            //     Ok(_) => {},
-            //     Err(err) => {panic!("{}",err)},
-            // }
-            kinfo!("ustack map");
-        }
-
-        let trap_frame = unsafe { RawSinglePage::try_new_zeroed().unwrap() as *mut TrapFrame };
-        // procdata.pagetable.as_mut().unwrap()
-        //     .map_pages(
-        //         VirtAddr::from(TRAPFRAME),
-        //         PGSIZE,
-        //         PhysAddr::try_from(trap_frame as usize).unwrap(),
-        //         PteFlag::R | PteFlag::W,
-        //     );
-
-        // // 分配进程页表
-        //             match PageTable::alloc_proc_pagetable(procdata.trapframe as usize) {
-        //                 Some(pagetable) => procdata.pagetable = Some(pagetable),
-        //                 None => {
-        //                     unsafe {
-        //                         RawSinglePage::from_raw_and_drop(procdata.trapframe as *mut u8);
-        //                     }
-        //                     return None;
-        //                 }
-        //             }
+        let trapframe_pa = unsafe { RawSinglePage::try_new_zeroed().unwrap() as usize};
+        kinfo!("set trapframe map {:?} {:x}",VirtAddr::from(trapframe_from_tid(tid)),procdata.pagetable.as_mut().unwrap().as_satp());
+        match procdata.pagetable.as_mut().unwrap()
+                    .map_pages(
+                        VirtAddr::from(trapframe_from_tid(tid)),
+                        PAGE_SIZE,
+                        PhysAddr::try_from(trapframe_pa).unwrap(),
+                        PteFlag::R | PteFlag::W,
+                    ) {
+            Ok(_) => {},
+            Err(_) => {panic!("task trapframe error")},
+        };
+        let ustack_bottom =ustack_bottom_from_tid(ustack_base, pos);
+        let trapframe =unsafe {&mut *(trapframe_pa as *mut TrapFrame)};
+        trapframe.epc = entry;
+        trapframe.sp = ustack_bottom + USER_STACK_SIZE;
+        trapframe.kernel_satp = satp::read();
+        // current kernel stack's content is cleaned
+        // after returning to the kernel space
+        trapframe.kernel_sp = kstack.kstack_base + KERNEL_STACK_SIZE;
+        trapframe.kernel_trap = user_trap as usize;
+        trapframe.kernel_hartid = unsafe { CpuManager::cpu_id() };
+        kinfo!("new {:?} \n {:?}",trapframe, unsafe { proc.data.get_mut().trapframe.as_ref().unwrap() });
         let mut context = Context::new();
         context.set_ra(fork_ret as usize);
         context.set_sp(kstack.kstack_base + KERNEL_STACK_SIZE);
@@ -174,32 +165,92 @@ impl Task {
         Self {
             process,
             kstack,
+            ustack_base,
             tid,
             pos,
-            inner: UnsafeCell::new(TaskControlInner {
-                res: Some(res),
-                trapframe: trap_frame,
+            inner: SpinLock::new(TaskControlInner {
+                trapframe: trapframe_pa as _,
                 task_context: context,
                 task_status: TaskStatus::Ready,
                 exit_code: None,
-            }),
+            },""),
+        }
+    }
+    pub fn from(process: Option<*mut Process>, pos:usize, ustack_base: usize, ptrapframe: *mut TrapFrame) -> Self {
+        let tid = TID_ALLOCATOR.lock().tid_alloc();
+        kinfo!("new alloc tid:{}", tid);
+        let kstack = unsafe { kstack_alloc(tid) };
+        let proc = unsafe { &mut *process.unwrap() };
+        let procdata = proc.data.get_mut();
+        kinfo!("set trapframe");
+        let ctrapframe = unsafe { RawSinglePage::try_new_zeroed().unwrap() as usize};
+        kinfo!("map {:?} {:x}",VirtAddr::from(trapframe_from_tid(tid)),procdata.pagetable.as_mut().unwrap().as_satp());
+        match procdata.pagetable.as_mut().unwrap()
+                    .map_pages(
+                        VirtAddr::from(trapframe_from_tid(tid)),
+                        PAGE_SIZE,
+                        PhysAddr::try_from(ctrapframe).unwrap(),
+                        PteFlag::R | PteFlag::W,
+                    ) {
+            Ok(_) => {},
+            Err(_) => {panic!("task trapframe error")},
+        };
+        let trapframe =unsafe {(ctrapframe as *mut TrapFrame).as_mut().unwrap()};
+        unsafe {
+            ptr::copy_nonoverlapping(ptrapframe, ctrapframe as *mut TrapFrame, 1);
+        }
+        trapframe.a0 = 0;
+        trapframe.kernel_sp = kstack.kstack_base + KERNEL_STACK_SIZE;
+        kinfo!("fork {:?} \n {:?}",trapframe,unsafe {& *proc.data.get_mut().trapframe });
+        let mut context = Context::new();
+        context.set_ra(fork_ret as usize);
+        context.set_sp(kstack.kstack_base + KERNEL_STACK_SIZE);
+
+        Self {
+            process,
+            kstack,
+            ustack_base,
+            tid,
+            pos,
+            inner: SpinLock::new(TaskControlInner {
+                trapframe: ctrapframe as _,
+                task_context: context,
+                task_status: TaskStatus::Ready,
+                exit_code: None,
+            },""),
         }
     }
     pub fn get_context(&mut self) -> *mut Context {
-        &mut self.inner.get_mut().task_context as *mut Context
+        &mut self.inner.lock().task_context as *mut Context
+    }
+    pub fn set_status(&self, status: TaskStatus){
+        self.inner.lock().task_status = status;
+    }
+    pub fn get_kstack_bottom(&self)-> usize {
+        self.kstack.kstack_base
+    }
+    pub fn get_trap_frame(&self) -> &'static mut TrapFrame {
+        self.inner.lock().get_trap_frame()
     }
 }
 
 impl Drop for Task {
     fn drop(&mut self) {
-        TID_ALLOCATOR.lock().tid_dealloc(self.tid);
-        kinfo!("dealloc tid:{}", self.tid);
+        let tid = self.tid;
+        let proc = unsafe { &mut *self.process.unwrap() };
+        let procdata = proc.data.get_mut();
+        kinfo!("free trapframe unmap {:?} {:x}",VirtAddr::from(trapframe_from_tid(tid)),procdata.pagetable.as_mut().unwrap().as_satp());
+        
+        procdata.pagetable.as_mut().unwrap().uvm_unmap(trapframe_from_tid(tid).into(), 1, false);
+        TID_ALLOCATOR.lock().tid_dealloc(tid);
+        kinfo!("dealloc tid:{}", tid);
     }
 }
 
+/// Return (bottom, top) of a kernel stack in kernel space.
 #[inline]
-fn kstack_by_tid(tid: usize) -> (usize,usize) {
-    let kstack_bottom = Into::<usize>::into(TRAMPOLINE) - (NPROC + 1) * (KERNEL_STACK_SIZE + PAGE_SIZE) - (tid + 1) * KERNEL_STACK_SIZE;
+fn kernel_stack_position_by_tid(tid: usize) -> (usize,usize) {
+    let kstack_bottom = Into::<usize>::into(TRAMPOLINE) - (tid + 4 + NPROC) * (KERNEL_STACK_SIZE + PAGE_SIZE) ;
     let kstack_top = kstack_bottom + KERNEL_STACK_SIZE;
     (kstack_bottom,kstack_top)
 }
@@ -209,6 +260,7 @@ fn ustack_bottom_from_tid(ustack_base: usize, pos: usize) -> usize {
     ustack_base + pos * (PAGE_SIZE + USER_STACK_SIZE)
 }
 /// get the trapframe ptr in user space by tid
-fn trapframe_from_tid(tid: usize) -> ConstAddr {
-    TRAPFRAME.const_sub(tid * PAGE_SIZE)
+#[inline]
+pub fn trapframe_from_tid(tid: usize) -> ConstAddr {
+    TRAPFRAME.const_sub((NPROC + 1) * (PAGE_SIZE + PAGE_SIZE) + tid * (PAGE_SIZE + PAGE_SIZE))
 }
